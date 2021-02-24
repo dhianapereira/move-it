0858b392cc8daeb21a879b282c9787a6fbee1e84
Dhiana <dhianaslv@outlook.com>
2021-02-24 19:35:20 -0300

    initial commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+    return (
+        <div>
+            <img src="https://github.com/dhianaslv.png" alt="Imagem de perfil"/>
+            <div>
+                <strong>Dhiana Pereira</strong>
+                <p>Level 1</p>
+            </div>
+        </div>
+    );
+}
